2bd003e1c4b0dab2bde43299ab83e94a4a691626
Viet Anh Duong (Percy) <anhd@sfu.ca>
2022-03-09 18:48:23 -0800

    modelize with nke data add on

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,8 +5,10 @@ import matplotlib.pyplot as plt
 from forex_python.converter import CurrencyRates
 import seaborn as sns
 
-def read_file(filename):
-    return pd.read_csv(filename, parse_dates=[0])
+def read_file(filename1, filename2):
+    ads_data = pd.read_csv(filename1, parse_dates=[0])
+    nke_data = pd.read_csv(filename2, parse_dates=[0])
+    return ads_data, nke_data
 
 def currency_rates(data):
     c = CurrencyRates()
@@ -14,26 +16,29 @@ def currency_rates(data):
     return data
 
 def exchange_currency(data):
-    data['Open_USD'] = data.apply(lambda x: x.Open*x.exchange_rate, axis=1)
-    data['Close_USD'] = data.apply(lambda x: x.Close*x.exchange_rate, axis=1)
-    data['CloseAdj_USD'] = data.apply(lambda x: x['Adj Close']*x.exchange_rate, axis=1)
+    data['Open'] = data.apply(lambda x: x.Open*x.exchange_rate, axis=1)
+    data['Close'] = data.apply(lambda x: x.Close*x.exchange_rate, axis=1)
+    data['Adj Close'] = data.apply(lambda x: x['Adj Close']*x.exchange_rate, axis=1)
     return data
 
-def plot_price_history(data):
+def plot_price_history(data, s):
     sns.set_theme()
     sns.set_style('darkgrid')
     plt.figure(figsize=(16,8))
-    plt.plot(data['Date'], data['Close_USD'], label='Close Price History', linewidth=4)
-    plt.plot(data['Date'], data['Open_USD'], label='Open Price History')
-    plt.plot(data['Date'] ,data['CloseAdj_USD'], label='Adj Close Price History')
+    plt.plot(data['Date'], data['Close'], label='Close Price History', linewidth=4)
+    plt.plot(data['Date'], data['Open'], label='Open Price History')
+    plt.plot(data['Date'] ,data['Adj Close'], label='Adj Close Price History')
     plt.grid(True)
     plt.xlabel('Date')
     plt.ylabel('Price History in USD')
     plt.legend()
     plt.xticks(rotation=65)
-    plt.title('Adidas Price History from March 2021 to March 2022')
-    plt.savefig('./output/ADS_price_history.png')
+    plt.title(s+' Price History from March 2021 to March 2022')
+    plt.savefig('./output/'+s+'_price_history.png')
     plt.show()
+
+def take_max(data):
+    return data.loc[data['Close'].idxmax()]
 
 def split_data(stock, lookback):
     data_raw = stock.to_numpy() # convert to numpy array
@@ -51,17 +56,26 @@ def split_data(stock, lookback):
     return [x_train, y_train, x_test, y_test]
 
 
-def main(filename):
+def main(filename1, filename2):
     print('This should take roughly 5 minutes, please wait!')
-    data = read_file(filename)
-    data = currency_rates(data)
-    data = exchange_currency(data)
-    data.to_csv('./data/ads_preprocessing.csv')
-    plot_price_history(data)
+    ads_data, nke_data = read_file(filename1, filename2)
+    ads_data = currency_rates(ads_data)
+    ads_data = exchange_currency(ads_data)
+    if len(filename1) < 10:
+        output1 = 'ads'
+        output2 = 'nke'  
+    else:
+        output1 = 'ads_monthly'
+        output2 = 'nke_monthly'
+    ads_data.to_csv('./data/'+output1+'_preprocessing.csv')
+    plot_price_history(ads_data, output1)
+    plot_price_history(nke_data, output2)
+    print('ADS:\n', take_max(ads_data))
+    print('NKE:\n', take_max(nke_data))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Please use the following command: python3 data_preprocessing.py ./data/ADS.DE.csv')
+    if len(sys.argv) != 3:
+        print('Example command: python3 data_preprocessing.py ./data/<filename1>.csv ./data/<filename2>.csv')
     else:
-        main(sys.argv[1])
+        main(sys.argv[1], sys.argv[2])
--- train.py
+++ train.py
@@ -14,8 +14,10 @@ lookback = 20
 sns.set_theme()
 sns.set_style('darkgrid')
 
-def read_file(file_name):
-    return pd.read_csv(file_name, parse_dates=[0])
+def read_file(filename1, filename2):
+    ads_data = pd.read_csv(filename1, parse_dates=[0])
+    nke_data = pd.read_csv(filename2, parse_dates=[0])
+    return ads_data, nke_data
 
 def train(model, X_train, y_train_gru):
     criterion = nn.MSELoss(reduction='mean')
@@ -50,22 +52,22 @@ def result(model, scaler, X_test, y_train_pred, y_train_gru, y_test_gru):
     y_test_gru = scaler.inverse_transform(y_test_gru.detach().numpy())
     return y_train_gru, y_train_pred, y_test_gru, y_test_pred
 
-def plot_training_loss(hist):
+def plot_training_loss(hist, s):
     plt.plot(hist, label='Training loss')
     plt.legend()
-    plt.savefig('./output/training_loss.png')
+    plt.savefig('./output/'+s+'_training_loss.png')
     plt.show()
 
-def plot_prediction(data, y_test_gru, y_test_pred):
+def plot_prediction(data, y_test_gru, y_test_pred, s):
     figure, axes = plt.subplots(figsize=(15, 6))
     axes.xaxis_date()
-    axes.plot(data[len(data)-len(y_test_gru):].index, y_test_gru, color='red', label='Real ADS Stock Price')
-    axes.plot(data[len(data)-len(y_test_gru):].index, y_test_pred, color='blue', label='Predicted ADS Stock Price')
-    plt.title('Adidas Stock Price Prediction')
+    axes.plot(data[len(data)-len(y_test_gru):].index, y_test_gru, color='red', label='Real '+s+' Stock Price')
+    axes.plot(data[len(data)-len(y_test_gru):].index, y_test_pred, color='blue', label='Predicted '+s+' Stock Price')
+    plt.title(s+' Stock Price Prediction')
     plt.xlabel('Time')
-    plt.ylabel('Adidas Stock Price')
+    plt.ylabel('Stock Price')
     plt.legend()
-    plt.savefig('./output/ads_pred.png')
+    plt.savefig('./output/'+s+'_pred.png')
     plt.show()
 
 def score(y_train_gru, y_train_pred, y_test_gru, y_test_pred):
@@ -73,23 +75,33 @@ def score(y_train_gru, y_train_pred, y_test_gru, y_test_pred):
     test_score = np.sqrt(mean_squared_error(y_test_gru[:, 0], y_test_pred[:, 0]))
     return train_score, test_score
 
-def main(filename):
-    data = read_file(filename)
+def main(filename1, filename2):
+    ads_data, nke_data = read_file(filename1, filename2)
     model = GRU(input_dim=input_dim, hidden_dim=hidden_dim, output_dim=output_dim, num_layers=num_layers)
-    price = data[['Close']]
+    price1 = ads_data[['Close']]
+    price2 = nke_data[['Close']]
     scaler = MinMaxScaler(feature_range=(-1, 1))
-    price['Close'] = scaler.fit_transform(price['Close'].values.reshape(-1, 1))
-    X_train, y_train_gru, X_test, y_test_gru = pass_to_tensor(price, lookback)
-    hist, y_train_pred = train(model, X_train, y_train_gru)
-    plot_training_loss(hist)
-    y_train_gru, y_train_pred, y_test_gru, y_test_pred = result(model, scaler, X_test, y_train_pred, y_train_gru, y_test_gru)
-    train_score, test_score = score(y_train_gru, y_train_pred, y_test_gru, y_test_pred)
-    print('Train Score: %.2f RMSE' % (train_score))
-    print('Test Score: %.2f RMSE' % (test_score))
-    plot_prediction(data, y_test_gru, y_test_pred)
+    price1['Close'] = scaler.fit_transform(price1['Close'].values.reshape(-1, 1))
+    price2['Close'] = scaler.fit_transform(price2['Close'].values.reshape(-1, 1))
+    X1_train, y1_train_gru, X1_test, y1_test_gru = pass_to_tensor(price1, lookback)
+    X2_train, y2_train_gru, X2_test, y2_test_gru = pass_to_tensor(price2, lookback)
+    hist1, y1_train_pred = train(model, X1_train, y1_train_gru)
+    hist2, y2_train_pred = train(model, X2_train, y2_train_gru)
+    plot_training_loss(hist1, 'ads')
+    plot_training_loss(hist2, 'nke')
+    y1_train_gru, y1_train_pred, y1_test_gru, y1_test_pred = result(model, scaler, X1_test, y1_train_pred, y1_train_gru, y1_test_gru)
+    y2_train_gru, y2_train_pred, y2_test_gru, y2_test_pred = result(model, scaler, X2_test, y2_train_pred, y2_train_gru, y2_test_gru)
+    train1_score, test1_score = score(y1_train_gru, y1_train_pred, y1_test_gru, y1_test_pred)
+    train2_score, test2_score = score(y2_train_gru, y2_train_pred, y2_test_gru, y2_test_pred)
+    print('Train ADS Score: %.2f RMSE' % (train1_score))
+    print('Test ADS Score: %.2f RMSE' % (test1_score))
+    print('Train NKE Score: %.2f RMSE' % (train2_score))
+    print('Test NKE Score: %.2f RMSE' % (test2_score))
+    plot_prediction(ads_data, y1_test_gru, y1_test_pred, 'ads')
+    plot_prediction(nke_data, y2_test_gru, y2_test_pred, 'nke')
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print('Please use the following command: python3 train.py ./data/ads_preprocessing.csv')
+    if len(sys.argv) != 3:
+        print('Example command: python3 train.py ./data/<filename1>.csv ./data/<filename2>.csv')
     else:
-        main(sys.argv[1])
+        main(sys.argv[1], sys.argv[2])
